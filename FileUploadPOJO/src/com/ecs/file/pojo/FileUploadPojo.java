0c578687e09c84465d876258fed8cff86120b0e1
nithinkarthick <nithinmatrix@gmail.com>
2025-01-23 11:45:45 +0530

    inital_commit1

---
+++ FileUploadPOJO/src/com/ecs/file/pojo/FileUploadPojo.java
@@ -0,0 +1,229 @@
+package com.ecs.file.pojo;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class FileUploadPojo implements Serializable{
+
+		/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	
+		private Long fileUploadId;
+	    private String userFile;
+	    private String docFile;
+	    private Integer status;
+	    private Long userId;
+	    private Long lastUserId;
+	    private String workflow;
+	    private String error;
+	    private Integer stamp;
+	    private String fileMaskingStatus;
+	    private Integer pageCount;
+	    private String docFormat;
+	    private String fileExt;
+	    private Long processedFileSize;
+	    private Long originalFileSize;
+	    private Integer timeTakenInSec;
+	    private String userIp;
+	    private String txnId;
+	    private String remark;
+	    private String docType;
+	    private Integer totalMaskCount;
+	    private String basePath;
+	    private Integer isAadhaarData;
+	    private String fullPath;
+	    private String remarks1;
+	    private String aadhaarNoMasked;
+	    private String relationId;
+	    private Date insertDate;
+	    private Date modifiedDate;
+	    private String baseFolder;
+		public Long getFileUploadId() {
+			return fileUploadId;
+		}
+		public void setFileUploadId(Long fileUploadId) {
+			this.fileUploadId = fileUploadId;
+		}
+		public String getUserFile() {
+			return userFile;
+		}
+		public void setUserFile(String userFile) {
+			this.userFile = userFile;
+		}
+		public String getDocFile() {
+			return docFile;
+		}
+		public void setDocFile(String docFile) {
+			this.docFile = docFile;
+		}
+		public Integer getStatus() {
+			return status;
+		}
+		public void setStatus(Integer status) {
+			this.status = status;
+		}
+		public Long getUserId() {
+			return userId;
+		}
+		public void setUserId(Long userId) {
+			this.userId = userId;
+		}
+		public Long getLastUserId() {
+			return lastUserId;
+		}
+		public void setLastUserId(Long lastUserId) {
+			this.lastUserId = lastUserId;
+		}
+		public String getWorkflow() {
+			return workflow;
+		}
+		public void setWorkflow(String workflow) {
+			this.workflow = workflow;
+		}
+		public String getError() {
+			return error;
+		}
+		public void setError(String error) {
+			this.error = error;
+		}
+		public Integer getStamp() {
+			return stamp;
+		}
+		public void setStamp(Integer stamp) {
+			this.stamp = stamp;
+		}
+		public String getFileMaskingStatus() {
+			return fileMaskingStatus;
+		}
+		public void setFileMaskingStatus(String fileMaskingStatus) {
+			this.fileMaskingStatus = fileMaskingStatus;
+		}
+		public Integer getPageCount() {
+			return pageCount;
+		}
+		public void setPageCount(Integer pageCount) {
+			this.pageCount = pageCount;
+		}
+		public String getDocFormat() {
+			return docFormat;
+		}
+		public void setDocFormat(String docFormat) {
+			this.docFormat = docFormat;
+		}
+		public String getFileExt() {
+			return fileExt;
+		}
+		public void setFileExt(String fileExt) {
+			this.fileExt = fileExt;
+		}
+		public Long getProcessedFileSize() {
+			return processedFileSize;
+		}
+		public void setProcessedFileSize(Long processedFileSize) {
+			this.processedFileSize = processedFileSize;
+		}
+		public Long getOriginalFileSize() {
+			return originalFileSize;
+		}
+		public void setOriginalFileSize(Long originalFileSize) {
+			this.originalFileSize = originalFileSize;
+		}
+		public Integer getTimeTakenInSec() {
+			return timeTakenInSec;
+		}
+		public void setTimeTakenInSec(Integer timeTakenInSec) {
+			this.timeTakenInSec = timeTakenInSec;
+		}
+		public String getUserIp() {
+			return userIp;
+		}
+		public void setUserIp(String userIp) {
+			this.userIp = userIp;
+		}
+		public String getTxnId() {
+			return txnId;
+		}
+		public void setTxnId(String txnId) {
+			this.txnId = txnId;
+		}
+		public String getRemark() {
+			return remark;
+		}
+		public void setRemark(String remark) {
+			this.remark = remark;
+		}
+		public String getDocType() {
+			return docType;
+		}
+		public void setDocType(String docType) {
+			this.docType = docType;
+		}
+		public Integer getTotalMaskCount() {
+			return totalMaskCount;
+		}
+		public void setTotalMaskCount(Integer totalMaskCount) {
+			this.totalMaskCount = totalMaskCount;
+		}
+		public String getBasePath() {
+			return basePath;
+		}
+		public void setBasePath(String basePath) {
+			this.basePath = basePath;
+		}
+		public Integer getIsAadhaarData() {
+			return isAadhaarData;
+		}
+		public void setIsAadhaarData(Integer isAadhaarData) {
+			this.isAadhaarData = isAadhaarData;
+		}
+		public String getFullPath() {
+			return fullPath;
+		}
+		public void setFullPath(String fullPath) {
+			this.fullPath = fullPath;
+		}
+		public String getRemarks1() {
+			return remarks1;
+		}
+		public void setRemarks1(String remarks1) {
+			this.remarks1 = remarks1;
+		}
+		public String getAadhaarNoMasked() {
+			return aadhaarNoMasked;
+		}
+		public void setAadhaarNoMasked(String aadhaarNoMasked) {
+			this.aadhaarNoMasked = aadhaarNoMasked;
+		}
+		public String getRelationId() {
+			return relationId;
+		}
+		public void setRelationId(String relationId) {
+			this.relationId = relationId;
+		}
+		public Date getInsertDate() {
+			return insertDate;
+		}
+		public void setInsertDate(Date insertDate) {
+			this.insertDate = insertDate;
+		}
+		public Date getModifiedDate() {
+			return modifiedDate;
+		}
+		public void setModifiedDate(Date modifiedDate) {
+			this.modifiedDate = modifiedDate;
+		}
+		public String getBaseFolder() {
+			return baseFolder;
+		}
+		public void setBaseFolder(String baseFolder) {
+			this.baseFolder = baseFolder;
+		}
+	    
+	    
+	    
+	    
+	    
+}
