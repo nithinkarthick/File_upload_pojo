b1f888349f92e7d8d9b3d6776ff10a78259d3c96
nithinkarthick <nithinmatrix@gmail.com>
2025-01-23 12:09:26 +0530

    added name2 in pojo

--- FileUploadPOJO/src/com/ecs/file/pojo/FileUploadPojo.java
+++ FileUploadPOJO/src/com/ecs/file/pojo/FileUploadPojo.java
@@ -43,8 +43,16 @@ public class FileUploadPojo implements Serializable{
 	    private String baseFolder;
 	    
 	    private String name1;
+	    private String name2;
 	    
 	    
+	    
+		public String getName2() {
+			return name2;
+		}
+		public void setName2(String name2) {
+			this.name2 = name2;
+		}
 		public String getName1() {
 			return name1;
 		}
