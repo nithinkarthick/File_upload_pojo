1c6d2d0a094dc43380d9289f76adaf6163f69990
nithinkarthick <nithinmatrix@gmail.com>
2025-01-23 12:03:13 +0530

    added a name parameter

--- FileUploadPOJO/src/com/ecs/file/pojo/FileUploadPojo.java
+++ FileUploadPOJO/src/com/ecs/file/pojo/FileUploadPojo.java
@@ -41,6 +41,16 @@ public class FileUploadPojo implements Serializable{
 	    private Date insertDate;
 	    private Date modifiedDate;
 	    private String baseFolder;
+	    
+	    private String name1;
+	    
+	    
+		public String getName1() {
+			return name1;
+		}
+		public void setName1(String name1) {
+			this.name1 = name1;
+		}
 		public Long getFileUploadId() {
 			return fileUploadId;
 		}
